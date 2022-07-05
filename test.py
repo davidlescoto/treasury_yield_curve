89f36742a59050efdd2c990300038506696dfd7e
davidlescoto <davidlescoto@hotmail.com>
2022-07-04 23:13:37 -0600

    deleted test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from yield_curve import Yield_Curve
-from datetime import date
-data = Yield_Curve().data
-#rate = Yield_Curve().svensson_rate()
-curve  = Yield_Curve().svensson_curve(date = date(2022,6,24))
